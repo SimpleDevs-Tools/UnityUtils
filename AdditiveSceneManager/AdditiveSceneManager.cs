f95c04fc73e57da7699ab12805417b42c74163bc
rkim-test <246917365+rkim-test@users.noreply.github.com>
2026-06-12 15:05:08 -0400

    added a 'switchscene()' function

--- AdditiveSceneManager/AdditiveSceneManager.cs
+++ AdditiveSceneManager/AdditiveSceneManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Events;
+using System.Linq;
 
 public class AdditiveSceneManager : MonoBehaviour
 {
@@ -26,6 +27,7 @@ public class AdditiveSceneManager : MonoBehaviour
     public List<string> scene_names;
     private Dictionary<string, Object> sceneDict;
     private List<string> activeScenes = new List<string>();
+    private string switchSceneName = null;
 
     [Header("=== Reference Management ===")]
     public List<Ref> references = new List<Ref>();
@@ -45,13 +47,20 @@ public class AdditiveSceneManager : MonoBehaviour
     }
     #endif
 
-    // Awake
+    // ===========================================
+    // On Awake - we need to intiialize everything
+    // ===========================================
     private void Awake() {
         Instance = this;
+        switchSceneName = null;
         InitializeScenes();
         InitializeRefs();
     }
 
+    // ===========================================
+    // Called during `Awake()`. Ensure that the scene manager calls `OnSceneLoaded` and `OnSceneUnloaded` whenever a scene change occurs
+    // This enables us to control certain events, such as if certain events should be invoked upon scenes changing.
+    // ===========================================
     private void InitializeScenes() {
         // Load listeners
         SceneManager.sceneLoaded += OnSceneLoaded;
@@ -60,45 +69,86 @@ public class AdditiveSceneManager : MonoBehaviour
         activeScenes = new List<string>();
     }
 
-    private void InitializeRefs()
-    {
+    // ===========================================
+    // If we need to pass any refs to new scenes, then we do so here.
+    // You can also technically use Singleton logic with your components, so this isn't strictly necessary.
+    // ===========================================
+    private void InitializeRefs() {
         refDict = new Dictionary<string, Object>();
         foreach(Ref r in references) refDict.Add(r.key, r.reference);
     }
 
-    // Add Scene from `_scenes` to scene
-    public void LoadScene(string query, LoadSceneMode mode) {
+    // ===========================================
+    // Call this function if you want to load a scene by name. This variant forces you to define a load scene mode.
+    // The overload version doesn't require the 2nd parameter and assumes you want to add a scene additively.
+    // ===========================================
+    public bool LoadScene(string query, LoadSceneMode mode) {
+        // Check if our scene is actually one we have reference to. We can't do anything if we don't have a ref to it.
         if(!scene_names.Contains(query)) {
             Debug.LogError($"Query scene \"{query}\" doesn't exist in this scene manager!");
-            return;
+            return false;
         }
+        // If the scene is already loaded, then we don't additively add it again.
         if (activeScenes.Contains(query)) {
             Debug.LogError($"Query scene \"{query}\" is already loaded additively. Will not add scene again.");
-            return;
+            return false;
         }
+        // Assuming all checks pass, we safely load the scene.
         SceneManager.LoadScene(query, mode);
+        return true;
     }
-    public void LoadScene(string query) {
-        LoadScene(query, LoadSceneMode.Additive);
+    public bool LoadScene(string query) {
+        return LoadScene(query, LoadSceneMode.Additive);
     }
 
-    public void UnloadScene(string query) {
+    // ===========================================
+    // Call this function to unload a loaded scene.
+    // ===========================================
+    public bool UnloadScene(string query) {
+        // Check if our scene is actually one we have reference to. We can't do anything if we don't have a ref to it.
         if (!scene_names.Contains(query)) {
             Debug.LogError($"Query scene \"{query}\" doesn't exist in this scene manager!");
-            return;
+            return false;
         }
+        // If the scene is not loaded, then there's no point unloaded an unloaded scene
         if (!activeScenes.Contains(query)) {
             Debug.LogError($"Query scene \"{query}\" is not loaded.");
-            return;
+            return false;
         }
+        // Safety checks, we call load scene async. This is a bit safer, as `UnloadScene` is no longer recommended
         SceneManager.UnloadSceneAsync(query);
+        return true;
     }
 
-    public void ToggleScene(string query)
-    {
+    // ===========================================
+    // This is a more unique function that allows us to `toggle` scenes on-off. 
+    // I don't recommend doing this as memory issues can occur if you keep toggling scenes on-off, but it's here if needed
+    // ===========================================
+    public void ToggleScene(string query) {
         if (activeScenes.Contains(query)) UnloadScene(query);
         else LoadScene(query, LoadSceneMode.Additive);
     }
+
+    // ===========================================
+    // This is a more unique function that allows you to `switch` between additive scenes.
+    // Note that because scenes may load asynchronously (or however much "asynchronous" the main thread allows), 
+    // It's recommended that you attach listeners to `onSceneLoadedCallback` and `onSceneUnloadedCallback` for optimal event invokation.
+    // In this case, we must wait for the scene to be unloaded first before we load in the second one, to avoid potential singleton issues.
+    // We use a private string that's stored in memory for this, and then when `OnSceneUnloaded` is called it'll call `LoadScene`
+    // ===========================================
+    public void SwitchScenes(string toUnload, string toLoad) {
+        // Set a reference to the scene-to-load in memory
+        switchSceneName = toLoad;
+        // It' not the end of the world if we can't unload the scene of choice. We just have to handle if that case happens
+        if (!UnloadScene(toUnload)) {
+            // Failure to unload scene. We can just call `LoadScene` from here
+            switchSceneName = null;
+            LoadScene(toLoad, LoadSceneMode.Additive);
+            return;
+        }
+        // Getting here, we've unloaded the scene. We can now just rely on `OnSceneUnloaded` to update the scene.
+    }
+
 
     public void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
         SceneManager.SetActiveScene(scene);
@@ -111,14 +161,21 @@ public class AdditiveSceneManager : MonoBehaviour
         if (activeScenes.Contains(scene.name)) activeScenes.Remove(scene.name);
         Debug.Log($"Scene \"scene.name\" unloaded!");
         onSceneUnloadedCallback?.Invoke(scene.name);
+        // Unique case: is there a scene we need to switch to?
+        if (!string.IsNullOrEmpty(switchSceneName)) {
+            LoadScene(switchSceneName, LoadSceneMode.Additive);
+            switchSceneName = null;
+        }
     }
 
     public bool QuerySceneLoaded(string query) {
         return activeScenes.Contains(query);
     }
+    public bool QuerySceneReferenced(string query) {
+        return scene_names.Contains(query);
+    }
 
-    public bool TryGetRef(string query, out GameObject g)
-    {
+    public bool TryGetRef(string query, out GameObject g) {
         bool found = refDict.ContainsKey(query);
         g = found ? refDict[query] as GameObject : null;
         return found;
